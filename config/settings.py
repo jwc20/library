900bfa0af9cf0edab279cf22a3a0f17864cc6a3e
jwc20 <choij2015@gmail.com>
2023-02-17 12:08:18 +0900

    Configured Installed App for rest framework.

--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local
+    "books.apps.BooksConfig",
+    "api.apps.ApiConfig",
+
+    # 3rd party
+    "rest_framework",
+
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
-    "books",
+
 ]
 
 MIDDLEWARE = [
